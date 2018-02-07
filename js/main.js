58b5406951d471f4c3efbd28dd5c4f590aadbaf9
char502 <char502@hotmail.com>
2018-02-07 16:17:21 +0000

    revision on js types

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert("new web page");
+// alert('new web page')
+
+// var message = "hello"
+// console.log(typeof message)
+
+// alert(Number.MAX_VALUE)
+// alert(Number.MIN_VALUE)
+
+// alert(NaN === Nan)
+
+// alert(isNaN(NaN)) // true
+// alert(isNaN(100)) // false
+// alert(isNaN('100')) // false
+// alert(isNaN('blue')) // false
+alert(isNaN(false)) // true becomes 1 - false becomes 0
