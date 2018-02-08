0d56a1e73f5315c9fe0175aa7dea3a7a2a9f1828
char502 <char502@hotmail.com>
2018-02-08 12:34:55 +0000

    js info on loops

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@
 //   name: 'Ryan',
 //   age: 30,
 //   sayName: function () {
-//     return this.name;
+//     return this.name
 //   },
 //   likes: {
 //     movies: {
@@ -56,38 +56,139 @@
 //     },
 //     books: 'js Ninja'
 //   }
-// };
+// }
 
 // console.log('My fav film is: ' + person.likes.movies)
 
-//console.log('My fav film is: ' + person.likes["movies"] + ' and book ' + person.likes["books"]);
-//console.log(person["age"]);
+// console.log('My fav film is: ' + person.likes["movies"] + ' and book ' + person.likes["books"])
+// console.log(person["age"])
 
-//console.log(person.likes['movies'].three);
+// console.log(person.likes['movies'].three)
 
-//Unary operatos - only works on one value - when you see +/- etc, will try to perfom mathematical conversion,
+// Unary operatos - only works on one value - when you see +/- etc, will try to perfom mathematical conversion,
 // even if it is a string (will return NaN)
 
-// var age = 30;
-// age++; //postfix
-// console.log(age);
+// var age = 30
+// age++ //postfix
+// console.log(age)
 
-//Ternary Operators
+// Ternary Operators
 
-// variable = expression ? true : false;
+// variable = expression ? true : false
 
-// var num1 = 200;
-// var num2 = 100;
+// var num1 = 200
+// var num2 = 100
 
 // var result = (num1 > num2) ? 'That is correct' : 'That is wrong'
-// console.log(result);
+// console.log(result)
 
 // Compound assignment operator
 
-// var num = 10;
+// var num = 10
 // num += 10; // compound assignment operator (also use +/- and / etc)
 
+// comma operators
 
-comma operators
+// allows for more than one operation in a single statement
 
-allows for more than one operation in a single statement
+// var name = "Charlotte", 
+//     age = 30, 
+//     address = "England"
+
+// do while loop - runs at least once
+
+// do {
+//   statement
+// } while () expression
+
+// var num = 0
+
+// do {
+//     num++
+//     console.log('Number: ', num)
+// } while (num < 10)
+
+// while loop - evaluates the condition first then runs the loop
+
+// var num = 0
+
+// while (num < 5) {
+//   num++
+//   console.log('num: ' + num)
+// }
+
+// for in loop (works in js objects)
+
+// goes through each of the properties of an object and returns them
+
+// var person = {
+//   name: 'Charlotte',
+//   age: 40,
+//   address: 'England'
+// }
+
+// item - can call it anything you want
+
+// for (item in person) {
+//   alert(item)
+// }
+
+// also:
+
+// for (item in window) {
+//   console.log(item)
+// }
+// (to look at all the properties of the window object)
+
+// labelled break and continue statements:
+
+var num = 0
+
+for (var i = 0; i < 10; i++) {
+  if (i == 5) {
+    break
+  }
+  num++ // num being incremented together with the loop
+}
+console.log(num)
+// result = 5 - the break stement stops the loop at 5
+
+var num = 0
+
+for (var i = 0; i < 10; i++) {
+  if (i == 5) {
+    continue
+  }
+  num++ // num being incremented together with the loop
+}
+console.log(num)
+// result = 9
+// one incrementor did not occur (the fifth) due to the continue statement
+
+// nested for loops:
+
+var num = 0
+
+outerLabel: for (var i = 0; i < 10; i++) {
+  for (var ii = 0; ii < 10; ii++) {
+    if (i == 5 && ii == 5) {
+      continue outerLabel; // result is 95 as the one iteration it missed encountered the continue statement
+    }
+    num++
+  }
+}
+console.log(num)
+// result = 95
+
+
+var num = 0
+
+outerLabel: for (var i = 0; i < 10; i++) {
+  for (var ii = 0; ii < 10; ii++) {
+    if (i == 5 && ii == 5) {
+      break outerLabel; // result is 95 as the one iteration it missed encountered the continue statement
+    }
+    num++
+  }
+}
+console.log(num)
