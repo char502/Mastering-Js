12851271239062339eb9e3f585d5b997a0247750
char502 <char502@hotmail.com>
2018-02-10 18:07:24 +0000

    scope and execution context

--- js_notes.js
+++ js_notes.js
@@ -1,1 +1,22 @@
-//execution context and scope chain
+/** 
+execution context and scope chain
+
+Execution Context
+=================
+- Execution context of a variable or function defines what other data it has access to.
+- Each function call has its own execution context
+
+Global Execution Context
+========================
+- This is the outermost one.
+- In web browsers it is window object
+
+Scope Chain
+===========
+- When code is executed in context, a scope chain is created.
+- It's purpose is to provide access to all variables and functions needed to the execution context.
+- This chain continues as NEEDED until global context is reached - which is window object
+
+*/
+
+//No Block level scope
