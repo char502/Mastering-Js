92d4aff6edd607517d34e0bfbe79ac4630f1287d
char502 <char502@hotmail.com>
2018-02-11 17:32:28 +0000

    reverse and sort

--- arrays.js
+++ arrays.js
@@ -97,9 +97,89 @@ console.log(colors, "Removed Item: ", removedItem);
 // [ 'cyan', 'red', 'blue', 'green' ] 'Removed Item: ' 'purple'
 */
 
+
 /*
 Reordering methods - reverse(), sort()
 
+reverse()
+
+var values = [1, 2, 3, 4, 5];
+values.reverse();
+console.log(values); //[ 5, 4, 3, 2, 1 ]
+
+
+sort() - puts values in ascending order
+
+behind the scenes, converts the numbers to **STRINGS** then compares them so may get unexpected results when sorting numbers i.e.
+
+=====================
+
+var values = [1, 5, 9, 4, 7, 3, 10];
+console.log(values.sort()); // [ 1, 10, 3, 4, 5, 7, 9 ]
+
+===================================
+var values = ["apples", "banana", "orange", "mango", "fig"];
+console.log(values.sort());
+
+// [ 'apples', 'banana', 'fig', 'mango', 'orange' ]
+
+===================================
+
+****To ensure expected result - sort takes a function (defining the sorting method required) as it's argument****
+
+
+var values = [1, 5, 9, 4, 7, 3, 10];
+console.log(values.sort(compare)); // [ 1, 3, 4, 5, 7, 9, 10 ]
+
+function compare(val1, val2) {
+    return val1 - val2;
+}
+
+the function (passed as an argument) allows a mathematical comparison to be done on the array
+
+==================================
+
+This will not work however when comparing strings
+
+
+var values = ["apples", "banana", "orange", "mango", "fig"];
+console.log(values.sort(compare));
+
+// [ 'apples', 'banana', 'orange', 'mango', 'fig' ]
+
+function compare(val1, val2) {
+    return val1 - val2;
+}
+
+===
+
+var values = ["apples", "banana", "orange", "mango", "fig"];
+console.log(values.sort(compareStr));
+
+// [ 'apples', 'banana', 'fig', 'mango', 'orange' ]
+
+function compareStr(val1, val2) {
+    if (val1 < val2) {
+        return -1; // neither true nor false, waiting for the change
+    } else if(val1 > val2) {
+        return 1; // as per boolean true
+    } else {
+        return 0; // as per boolean false
+    }
+};
+
+switch the comparison operators around if want to compare the other way i.e. < becomes > and > becomes < (can then compare z to a rather then a to z)
+
+===========================================
+
+Can also be written this way (inserting a compare function as a callback  - numerical comparison example given:
+
+var values = [1, 5, 9, 4, 7, 3, 10];
+
+values.sort(function (a, b) {
+    return a - b;
+});
+console.log(values);
 
 
 */
