4313202581d4e5c03cae8dff180a7395cfb90ae2
char502 <char502@hotmail.com>
2018-02-28 22:22:24 +0000

    DOM, write, set and create elements

--- DOM_write_set_create elements.js
+++ DOM_write_set_create elements.js
@@ -6,7 +6,27 @@ DOM - write, set and create elements
 - set attribute
 - create element
 
+=======
 
+document.write("Hello world writing in document");
+
+var div = document.getElementById("root");
+//console.log(div.getAttribute("id"));
+
+div.setAttribute("id", "hahaha");
+console.log(div.getAttribute("id"));
+
+function myClick() {
+    var div = document.createElement("h1");
+    var text = "hello world";
+    var newH1 = document.body.appendChild(div);
+    newH1.innerText = text;
+}
+
+
+=====
+
+Finding elements in the DOM using selectors
 
 
 
